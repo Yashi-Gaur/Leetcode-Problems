a6125b40c3e14cf7a5aea596a6a36ec2420876cb
Yashi Gaur <85959390+Yashi-Gaur@users.noreply.github.com>
2025-12-01 23:51:06 +0530

    Time: 6 ms (5.27%) | Memory: 8.6 MB (20.79%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
     int reverse(int x) {
-        int temp = x;
-        int temp_n = 0;
-        int dig_1 = x%10;
-        cout << dig_1;
-        while(temp != 0) {
-            temp_n++;
-            if(temp_n > 9) {
-                if(abs(dig_1) > 2) return 0;
-            }
-            temp /= 10;
-        }
         int n = 0;
         int ans = 0;
-        int sign = 0;
-        if(x >= 0) sign = 1;
-        else sign = -1;
-        x *= sign;
 
         while(x!=0) {
             n++;
             int num = x%10;
             x = x/10;
-            if(n > 9) {
-                if(ans > 214748364) return 0;
+            if ((ans > INT_MAX / 10) || (ans < INT_MIN / 10)) {
+                return 0; // Return 0 if reversing x would cause overflow/underflow
             }
             ans = ans*10+num;
         }
-        return ans*sign;
+        return ans;
     }
 };
